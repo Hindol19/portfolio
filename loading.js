3fb8831a59083a6762fab13ad25866af209b51d8
Hindol19 <108117454+Hindol19@users.noreply.github.com>
2023-01-08 01:59:58 +0530

    First Commit

---
+++ loading.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	
+	setTimeout(function(){	
+		$('nav').removeClass('loading');
+	}, 5000);
+	setTimeout(function(){	
+		$('section').css('opacity', '1');
+	}, 4200);
+	
+});
